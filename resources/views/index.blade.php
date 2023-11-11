26510dccd7cd5dfbb1ffd273b3ad752f0d70188f
Dika1485 <ptiandika@gmail.com>
2023-11-11 21:17:11 +0700

    plus province in java

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="map" style="height: 100vh;"></div>
+    <div id="map-container">
+        <div id="provinceSelectorCard">
+            <label for="provinceSelector">Choose Province:</label>
+            <select id="provinceSelector" class="form-select" onchange="changeProvince()">
+                <option value="centralJava">Central Java</option>
+                <option value="westJava">West Java</option>
+                <option value="eastJava">East Java</option>
+                <option value="diy">Daerah Istimewa Yogyakarta (DIY)</option>
+                <option value="dkiJakarta">Daerah Khusus Ibukota (DKI) Jakarta</option>
+                <option value="banten">Banten</option>
+            </select>
+        </div>
+        <div id="map"></div>
+    </div>
 @endsection
 
 @push('scripts')
 <script>
-    const map = L.map('map', {
-        center: [-7.150975, 110.140259],
-        zoom: 8,
-    });
-
-    const bounds = L.latLngBounds([-9, 108], [-5, 112]); // batas geografis untuk Jawa Tengah
+    let shapefileLayer = L.layerGroup();
+    let shpfile;
+    const map = L.map('map');
+    let bounds=L.latLngBounds([-11, 95], [-6, 113]);;
 
     map.setMaxBounds(bounds);
     map.on('drag', function () {
@@ -20,9 +31,10 @@
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
-        minZoom: 8,
+        minZoom: 5,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
     function getRandomColor() {
         const letters = '0123456789ABCDEF';
         let color = '#';
@@ -32,56 +44,155 @@
         return color;
     }
 
-    const shpfile = new L.Shapefile("{{ asset('shapefile/BATAS_DESA_DESEMBER_2019_DUKCAPIL_JAWA_TENGAH.zip') }}", {
-    // const shpfile = new L.Shapefile("{{ asset('shapefile/BATAS KECAMATAN DESEMBER 2019 DUKCAPIL.zip') }}", {
-    // from https://www.indonesia-geospasial.com/2020/04/download-shapefile-shp-batas.html
-    // const shpfile = new L.Shapefile("{{ asset('shapefile/IDN_adm.zip') }}", {
-    // from https://www.diva-gis.org/gdata
-        onEachFeature: function (feature, layer) {
-            if (feature.properties) {
-                layer.bindPopup(
-                    Object.keys(feature.properties)
-                        .map(function (k) {
-                            return k + ': ' + feature.properties[k];
-                        })
-                        .join('<br />'),
-                    {
-                        maxHeight: 200
-                    }
-                );
-            }
-        },
-        style: function (feature) {
-            const randomColor = getRandomColor();
+    function changeProvince() {
+        const selectedProvince = document.getElementById('provinceSelector').value;
 
-            const style = {
-                fillColor: randomColor,
-                weight: 1,
-                opacity: 1,
-                color: 'white',
-                dashArray: '3',
-                fillOpacity: 0.7
-            };
-
-            return style;
+        if (shapefileLayer) {
+            shapefileLayer.clearLayers();
         }
-    });
 
-    shpfile.addTo(map);
+        const shapefilePath = getShapefilePath(selectedProvince);
+        shpfile = new L.Shapefile(shapefilePath, {
+            onEachFeature: function (feature, layer) {
+                if (feature.properties) {
+                    layer.bindPopup(
+                        Object.keys(feature.properties)
+                            .map(function (k) {
+                                return k + ': ' + feature.properties[k];
+                            })
+                            .join('<br />'),
+                        {
+                            maxHeight: 200
+                        }
+                    );
+                }
+            },
+            style: function (feature) {
+                const randomColor = getRandomColor();
 
-    shpfile.once('data:loaded', function () {
-        console.log('Shapefile loaded successfully');
-        shpfile.eachLayer(function (layer) {
-            shapefileLayer.addLayer(layer);
-            layer.bindPopup(createPopupContent(layer.feature.properties), {
-                maxHeight: 200
-            });
-            layer.on('click', function (e) {
-                e.target.openPopup();
-            });
+                const style = {
+                    fillColor: randomColor,
+                    weight: 1,
+                    opacity: 1,
+                    color: 'white',
+                    dashArray: '3',
+                    fillOpacity: 0.7
+                };
+
+                return style;
+            }
         });
-        shapefileLayer.addTo(map);
-    });
+
+        shpfile.addTo(map);
+        shapefileLayer = shpfile;
+        bounds = getBoundsByProvince(selectedProvince);
+        const minZoom = getMinZoomByProvince(selectedProvince);
+        tiles.removeFrom(map);
+
+        // const shapefileBounds = shpfile.getBounds();
+        // const center = shapefileBounds.getCenter();
+        // const zoom = calculateZoom(shapefileBounds, map.getSize());
+        // const minZoom = 8;
+
+        shpfile.once('data:loaded', function () {
+            console.log('Shapefile loaded successfully');
+            shpfile.eachLayer(function (layer) {
+                shapefileLayer.addLayer(layer);
+                layer.bindPopup(createPopupContent(layer.feature.properties), {
+                    maxHeight: 200
+                });
+                layer.on('click', function (e) {
+                    e.target.openPopup();
+                });
+            });
+            shapefileLayer.addTo(map);
+            updateMapProperties(bounds, minZoom);
+        });
+    }
+    function getBoundsByProvince(province) {
+        switch (province) {
+            case 'centralJava':
+                return L.latLngBounds([-9, 108], [-5, 112]);
+            case 'westJava':
+                return L.latLngBounds([-8, 106], [-5, 109]);
+            case 'eastJava':
+                return L.latLngBounds([-9, 110], [-7, 115]);
+            case 'diy':
+                return L.latLngBounds([-8.5, 110], [-7.5, 111]);
+            case 'dkiJakarta':
+                return L.latLngBounds([-6.5, 106.5], [-5, 107]);
+            case 'banten':
+                return L.latLngBounds([-8, 105], [-5, 107]);
+            default:
+                return L.latLngBounds([-11, 95], [-6, 113]);
+        }
+    }
+
+    function getMinZoomByProvince(province) {
+        switch (province) {
+            case 'centralJava':
+                return 8;
+            case 'westJava':
+                return 8;
+            case 'eastJava':
+                return 7;
+            case 'diy':
+                return 10;
+            case 'dkiJakarta':
+                return 9;
+            case 'banten':
+                return 9;
+            default:
+                return 8; // Default nilai minZoom
+        }
+    }
+
+    function getShapefilePath(province) {
+        switch (province) {
+            case 'centralJava':
+                return "{{ asset('shapefile/BATAS DESA DESEMBER 2019 DUKCAPIL JAWA TENGAH.zip') }}";
+            case 'westJava':
+                return "{{ asset('shapefile/BATAS DESA DESEMBER 2019 DUKCAPIL JAWA BARAT.zip') }}";
+            case 'eastJava':
+                return "{{ asset('shapefile/BATAS DESA DESEMBER 2019 DUKCAPIL JAWA TIMUR.zip') }}";
+            case 'diy':
+                return "{{ asset('shapefile/BATAS DESA DESEMBER 2019 DUKCAPIL DI YOGYAKARTA.zip') }}";
+            case 'dkiJakarta':
+                return "{{ asset('shapefile/BATAS DESA DESEMBER 2019 DUKCAPIL DKI JAKARTA.zip') }}";
+            case 'banten':
+                return "{{ asset('shapefile/BATAS DESA DESEMBER 2019 DUKCAPIL BANTEN.zip') }}";
+            default:
+                return "";
+        }
+    }
+
+    function calculateZoom(bounds, mapSize) {
+        const worldDim = 256;
+        const zoomMax = 19;
+        let zoom = 0;
+        while (worldDim < Math.max(mapSize.x, mapSize.y) && zoom < zoomMax) {
+            zoom++;
+            worldDim *= 2;
+        }
+        return zoom;
+    }
+
+    window.onload = function () {
+        // map.setView([-7.150975, 110.140259], 7);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            minZoom: 6,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        changeProvince();
+    };
+
+    function updateMapProperties(bounds, minZoom) {
+        map.options.minZoom = minZoom;
+        map.setMaxBounds(bounds);
+        map.fitBounds(bounds);
+    }
 
     function createPopupContent(properties) {
         let popupContent = '<table>';
@@ -91,29 +202,10 @@
         popupContent += '</table>';
         return popupContent;
     }
-
-    // function onLocationFound(e) {
-	// 	const radius = e.accuracy / 2;
-
-	// 	const locationMarker = L.marker(e.latlng).addTo(map)
-	// 		.bindPopup(`You are within ${radius} meters from this point`).openPopup();
-
-	// 	const locationCircle = L.circle(e.latlng, radius).addTo(map);
-    //     shpfile.eachLayer(function (layer) {
-    //         layer.on('click', function (event) {
-    //             // Tampilkan popup dengan detail dari Shapefile ketika lapisan diklik
-    //             layer.openPopup();
-    //         });
-    //     });
-	// }
-
-	// function onLocationError(e) {
-	// 	alert(e.message);
-	// }
-
-	// map.on('locationfound', onLocationFound);
-	// map.on('locationerror', onLocationError);
-
-	// map.locate({setView: true, maxZoom: 16});
+</script>
+<script>
+    $(document).ready(function() {
+        $('#provinceSelector').select2();
+    });
 </script>
 @endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>GIS of Village in Central Java</title>
+    <title>GIS of Village in Java</title>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
      integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
      crossorigin=""/>
-     <!-- Make sure you put this AFTER Leaflet's CSS -->
- <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <style>
+        #map-container {
+            position: relative;
+            height: 100vh;
+        }
+
+        #provinceSelectorCard {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            background-color: white;
+            padding: 10px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            z-index: 1000;
+        }
+
+        #map {
+            height: 100%;
+        }
+    </style>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
      integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
      crossorigin=""></script>
-     <script src="https://unpkg.com/shpjs@latest/dist/shp.js"></script>
-     <script src="{{ asset('js/leaflet.shpfile.js') }}"></script>
-     <script src="{{ asset('js/shp.js') }}"></script>
+    <script src="https://unpkg.com/shpjs@latest/dist/shp.js"></script>
+    <script src="{{ asset('js/leaflet.shpfile.js') }}"></script>
+    <script src="{{ asset('js/shp.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 </head>
 <body>
     <div class="container">
         @yield('content')
     </div>
 
-    @stack('scripts') <!-- Stack for additional JavaScript scripts -->
+    @stack('scripts')
 </body>
 </html>
